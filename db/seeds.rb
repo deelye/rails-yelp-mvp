d69585892cd33cd801b6c7986b676a8dd0133375
deelye <dee.yen.lye@gmail.com>
2020-02-19 15:54:57 +0000

    challenge completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all if Rails.env.development?
+
+restaurants = Restaurant.create([
+  { name: 'Pizza East', address: 'Shoreditch', phone_number: '7000 1234', category: 'italian' },
+  { name: 'Xu', address: 'Soho', phone_number: '7111 1234',category: 'chinese' },
+  { name: 'Frenchie', address: 'Covent Garden', phone_number: '7222 1234',category: 'french' },
+  { name: 'Roka', address: 'Aldwych', phone_number: '7333 1234',category: 'japanese' },
+  { name: 'Belgo', address: 'Kings Cross', phone_number: '7444 1234',category: 'belgian' }
+  ])
